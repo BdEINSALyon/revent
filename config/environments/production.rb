ff0cafabb52f62eafb1d6a014c0d4a0b028eaff6
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-02-18 12:52:28 +0100

    Add host for prod mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
       api_key: ENV['MAILGUN_KEY'],
